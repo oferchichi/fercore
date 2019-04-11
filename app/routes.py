e99bd2c1023d06717a8bd94139b3d76feca733b4
oferchichi <ferchichi.omar@gmail.com>
2019-04-11 13:17:03 +0200

    de

--- app/routes.py
+++ app/routes.py
@@ -319,20 +319,20 @@ def disablenodeinpool():
     print("[SIMCA][WORKFLOW][DISABLE NODE IN POOL] : Chargement du VS + Equipement")
     nodes = Nodes.query.filter_by(id=id).first()
     pools = Pools.query.filter_by(name=poolname).first()
-    vs = VirtualServer.query.filter_by(id=pools.vs_id).first()
-    equipement = Equipement.query.filter_by(id=vs.equipement_id).first()
-    f5disable = F5()
-    connx = f5disable.connexion(equipement.login, equipement.password, equipement.ip)
-    print("[SIMCA][WORKFLOW][DISABLE NODE IN POOL] : Desactivation du node {} dans le pool {}".format(nodes.name, poolname))
-    try:
-        etat = f5disable.suspendrePool(connx, poolname, nodes.fullname, "Common")
-        if etat == "erreur":
-            print("[SIMCA][WORKFLOW][DISABLE NODE IN POOL] : Erreur de desactivation")
-            status = "Erreur de desactivation"
-        else:
-            print("[SIMCA][WORKFLOW][DISABLE NODE IN POOL] : Descativation fait avec success")
-            status ="succes"
-    except Exception as e:
-        print("[SIMCA][WORKFLOW][DISABLE NODE IN POOL] : Rollback FAIT")
-        status = "rollback : {}".format(str(e))
-    return jsonify({"ETAT": status})
+    # vs = VirtualServer.query.filter_by(id=pools.vs_id).first()
+    # equipement = Equipement.query.filter_by(id=vs.equipement_id).first()
+    # f5disable = F5()
+    # connx = f5disable.connexion(equipement.login, equipement.password, equipement.ip)
+    # print("[SIMCA][WORKFLOW][DISABLE NODE IN POOL] : Desactivation du node {} dans le pool {}".format(nodes.name, poolname))
+    # try:
+    #     etat = f5disable.suspendrePool(connx, poolname, nodes.fullname, "Common")
+    #     if etat == "erreur":
+    #         print("[SIMCA][WORKFLOW][DISABLE NODE IN POOL] : Erreur de desactivation")
+    #         status = "Erreur de desactivation"
+    #     else:
+    #         print("[SIMCA][WORKFLOW][DISABLE NODE IN POOL] : Descativation fait avec success")
+    #         status ="succes"
+    # except Exception as e:
+    #     print("[SIMCA][WORKFLOW][DISABLE NODE IN POOL] : Rollback FAIT")
+    #     status = "rollback : {}".format(str(e))
+    return jsonify({"ETAT": pools.portService})
